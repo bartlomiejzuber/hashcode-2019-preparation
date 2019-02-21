0677852f5c6bc0c8d98df4bde8fd9dcdb0ee2443
Bart Zuber <bzuber@neworbit.co.uk>
2019-02-21 19:16:59 +0000

    first step

--- main.py
+++ main.py
@@ -1,1 +1,29 @@
 import math
+class TaskDefinition: 
+    rows:int
+    columns:int
+    minimumIngr:int
+    maxCells:int
+
+def getTaskDefinitions(fileLines):
+    taskDefinition = TaskDefinition()
+    definitions= fileLines[0]
+    fileLines.remove(fileLines[0])
+    taskDefinitionData = definitions.split(" ")
+    taskDefinition.rows = int(taskDefinitionData[0])
+    taskDefinition.columns = int(taskDefinitionData[1])
+    taskDefinition.minimumIngr = int(taskDefinitionData[2])
+    taskDefinition.maxCells = int(taskDefinitionData[3])
+    return taskDefinition
+statementsFolderPath = "C:\HashCodeStatements\\"
+fileNames = [
+    "a_example.in",
+    "b_small.in",
+    "c_medium.in",
+    "d_big.in"
+]
+with open(statementsFolderPath + fileNames[0], 'r') as myfile:
+    #data = myfile.read()
+    fileLines = myfile.readlines()
+taskDefinition = getTaskDefinitions(fileLines)
+print(f'{taskDefinition.columns}')
